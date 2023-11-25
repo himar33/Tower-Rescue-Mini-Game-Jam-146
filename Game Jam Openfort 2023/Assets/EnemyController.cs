a587d3e52f689c2662669b09e3751eb6146268a1
IsmaUPC <61043099+IsmaUPC@users.noreply.github.com>
2023-11-25 22:30:05 +0100

    Logica provisional de enemy

--- Game Jam Openfort 2023/Assets/EnemyController.cs
+++ Game Jam Openfort 2023/Assets/EnemyController.cs
@@ -5,32 +5,85 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-    public float velocidad = 5f;
+    public float vel = 5f;
     public Transform mFloorTransform;
+    public Transform mPlayerTransform;
+
+    [Range(1f,10f)]
+    public float radioMelee;  // Asigna el Slider desde el Inspector.
+    private SphereCollider collDetect;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        collDetect = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoverEnemigo();
+        MoveEnemy();
 
     }
-    void MoverEnemigo()
+    void MoveEnemy()
     {
         // Obtén la entrada del teclado o cualquier otra fuente de entrada.
-        float movimientoHorizontal = Input.GetAxis("Horizontal");
-        float movimientoVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float mvVertical = Input.GetAxis("Vertical");
 
         // Calcula la dirección del movimiento en el plano XZ.
-        Vector3 direccion = new Vector3(movimientoHorizontal, 0, movimientoVertical).normalized;
+        Vector3 dir = new Vector3(moveHorizontal, 0, mvVertical).normalized;
 
-        transform.Translate((mFloorTransform.right* direccion.x + mFloorTransform.forward* direccion.z)*velocidad*Time.deltaTime);
+        transform.Translate((mFloorTransform.right* dir.x + mFloorTransform.forward* dir.z)*vel*Time.deltaTime);
 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Alert();
+            Pursue();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //todo dejar de seguir
+            //Pursue();
+        }
+    }
+
+    public void Alert() 
+    {
+        // Obtiene las posiciones de los colliders.
+        Vector3 pos1 = transform.position;
+        Vector3 pos2 = mPlayerTransform.transform.position;
+
+        Debug.Log("¡Jugador detectado!");
+        if (Vector3.Distance(pos1, pos2) < radioMelee)
+        {
+            Melee();
+        }
+    }
+
+    public void Melee()
+    {
+        // Calcula la dirección hacia el jugador sin cambiar la rotación del enemigo.
+        Vector3 dir = (mPlayerTransform.transform.position - transform.position).normalized;
+
+        // Mueve el enemigo en la dirección calculada.
+        transform.Translate((mFloorTransform.right * dir.x + (mFloorTransform.forward * dir.z)) * (vel * 2) * Time.deltaTime, Space.World);
+
+    }
+    void Pursue()
+    {
+        // Calcula la dirección hacia el jugador sin cambiar la rotación del enemigo.
+        Vector3 dir = (mPlayerTransform.transform.position - transform.position).normalized;
+
+        // Mueve el enemigo en la dirección calculada.
+        transform.Translate((mFloorTransform.right * dir.x + mFloorTransform.forward * dir.z) * vel * Time.deltaTime, Space.World);
+     
     }
 }
