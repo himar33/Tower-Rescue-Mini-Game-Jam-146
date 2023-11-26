ff1911d7804786093040102ff1ddb4bbd1a89b06
Jowent <jtorrent00@gmail.com>
2023-11-26 14:01:42 +0100

    Lights and Better Preview Mask

--- Game Jam Openfort 2023/Assets/Scripts/MostrarCaminos/CRevealPath.cs
+++ Game Jam Openfort 2023/Assets/Scripts/MostrarCaminos/CRevealPath.cs
@@ -3,33 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CRevealPath : MonoBehaviour
 {
+    [Header("Generation")]
+    
+    [SerializeField]
+    CSpellScriptableObject mSpellInfo;
+    
     [SerializeField]
     Material mMaskMaterial;
 
     [SerializeField]
-    Material mPreviewMaterial;
+    LayerMask mRevealObjectMask;
     
     [SerializeField]
-    CSpellScriptableObject mSpellInfo;
-
-    [SerializeField]
-    LayerMask mRevealObjectMask;
+    LayerMask mFloorMask;
     
     [SerializeField]
     int mDrawMaskBufferLayer;
 
     [SerializeField]
     ObjectPooler mMaskObjectPooler;
+
+    [Header("Preview Mask")]
+
+    [SerializeField]
+    Material mPreviewMaterial;
+    
+    [SerializeField]
+    Color mPreviewValidColor;
+
+    [SerializeField]
+    Color mPreviewInvalidColor;
     
     // Drag positions from camera to world
-    Vector3 mInitWorldPos;
-    Vector3 mCurrentWorldPos;
+    Vector3 mInitShadowWorldPoint;
+    Vector3 mCurrentShadowWorldPoint;
+    Vector2 mMaskDimensions;
 
     GameObject mPreviewObject;
     Mesh mPreviewQuadMesh;
@@ -53,7 +68,7 @@ public class CRevealPath : MonoBehaviour
 
     public void Start()
     {
-        mInitWorldPos = Vector2.zero;
+        mInitShadowWorldPoint = Vector2.zero;
 
         mMaskObjectPooler.AddConstructor(CreateMaskGameobject);
     }
@@ -71,7 +86,7 @@ public class CRevealPath : MonoBehaviour
             
             if(Physics.Raycast(ClickRay, out RaycastHit HitInfo, Mathf.Infinity, mRevealObjectMask))
             {
-                mInitWorldPos = HitInfo.point;
+                mInitShadowWorldPoint = HitInfo.point;
                 mIsValidDrag = true;
             }
             else
@@ -81,18 +96,30 @@ public class CRevealPath : MonoBehaviour
             }
         }
 
-        if(mIsValidDrag && Input.GetKey(mClickKeycode))
+
+        if(Input.GetKey(mClickKeycode))
         {
             Ray DragRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             
             if(Physics.Raycast(DragRay, out RaycastHit HitInfo, Mathf.Infinity, mRevealObjectMask))
             {
+                mMaskDimensions = new Vector2()
+                {
+                    x = Mathf.Abs(mInitShadowWorldPoint.x - mCurrentShadowWorldPoint.x) ,
+                    y = Mathf.Abs(mInitShadowWorldPoint.y - mCurrentShadowWorldPoint.y)
+                };
+                
                 UpdatePreviewMesh();
 
                 mPreviewObject.SetActive(true);
 
-                mCurrentWorldPos = HitInfo.point;
+                mCurrentShadowWorldPoint = HitInfo.point;
                 mIsValidDrag = true;
+            }
+            else
+            {
+                mIsValidDrag = false;
+                UpdatePreviewMesh();
             }
         }
 
@@ -103,24 +130,46 @@ public class CRevealPath : MonoBehaviour
                 Mesh NewMesh = new Mesh();
                 NewMesh.name = "RevealQuad";
 
-                SetBoundMesh(ref NewMesh, mInitWorldPos, mCurrentWorldPos);
+                SetBoundMesh(ref NewMesh, mInitShadowWorldPoint, mCurrentShadowWorldPoint);
                 
                 var NewMaskObject = mMaskObjectPooler.GetPooledObject();
                 NewMaskObject.GetComponent<MeshFilter>().mesh = NewMesh;
-                NewMaskObject.GetComponent<CSpellRevealMask>().Config(mSpellInfo);
+
+                NewMaskObject.GetComponent<CSpellRevealMask>().ConfigSpell(mSpellInfo, mMaskDimensions);
+
+                Vector3 LightShadowPos = new Vector3()
+                {
+                    x = mInitShadowWorldPoint.x < mCurrentShadowWorldPoint.x ? 
+                        Mathf.Lerp(mInitShadowWorldPoint.x, mCurrentShadowWorldPoint.x, mSpellInfo.mLightPosition01.x) :
+                        Mathf.Lerp(mCurrentShadowWorldPoint.x, mInitShadowWorldPoint.x, mSpellInfo.mLightPosition01.x) ,
+                    
+                    y = mInitShadowWorldPoint.y < mCurrentShadowWorldPoint.y ? 
+                        Mathf.Lerp(mInitShadowWorldPoint.y, mCurrentShadowWorldPoint.y, mSpellInfo.mLightPosition01.y) :
+                        Mathf.Lerp(mCurrentShadowWorldPoint.y, mInitShadowWorldPoint.y, mSpellInfo.mLightPosition01.y) ,
+                    
+                    z = Mathf.Lerp(mInitShadowWorldPoint.z, mCurrentShadowWorldPoint.z, 0.5f)
+                };
+                
+                if(Physics.Raycast(LightShadowPos, LightShadowPos - Camera.main.transform.position, out RaycastHit LightHit, Mathf.Infinity, mFloorMask))
+                {
+                    float LightSize = (mMaskDimensions.x + mMaskDimensions.y) / 2;
+
+                    NewMaskObject.GetComponent<CSpellRevealMask>().ConfigLight(LightHit.point - Vector3.forward * LightSize / 4.0f, LightSize);
+                }
+                
                 NewMaskObject.SetActive(true);
             }
 
             mPreviewObject.SetActive(false);
 
-            mInitWorldPos = Vector2.zero;
+            mInitShadowWorldPoint = Vector2.zero;
         }
     }
 
     void ClampPositionToTopLeftCorner()
     {
-        mCurrentWorldPos.x = Math.Max(mCurrentWorldPos.x, mInitWorldPos.x);
-        mCurrentWorldPos.y = Math.Max(mCurrentWorldPos.y, mInitWorldPos.y);
+        mCurrentShadowWorldPoint.x = Math.Max(mCurrentShadowWorldPoint.x, mInitShadowWorldPoint.x);
+        mCurrentShadowWorldPoint.y = Math.Max(mCurrentShadowWorldPoint.y, mInitShadowWorldPoint.y);
     }
 
     void SetBoundMesh(ref Mesh aMesh, Vector3 aPointA, Vector3 aPointB)
@@ -164,16 +213,20 @@ public class CRevealPath : MonoBehaviour
             FilterComponent.mesh = mPreviewQuadMesh;
         }
 
-        SetBoundMesh(ref mPreviewQuadMesh, mInitWorldPos, mCurrentWorldPos);
+        SetBoundMesh(ref mPreviewQuadMesh, mInitShadowWorldPoint, mCurrentShadowWorldPoint);
 
         PREVIEW_MPB.SetTexture("_MainTex", mSpellInfo.mStencilTexture);
+        PREVIEW_MPB.SetFloat("_MaskWidth", mMaskDimensions.x);
+        PREVIEW_MPB.SetFloat("_MaskHeight", mMaskDimensions.y);
+        Debug.Log(mMaskDimensions.x);
+        PREVIEW_MPB.SetColor("_HighlightColor", mIsValidDrag ? mPreviewValidColor : mPreviewInvalidColor);
         mPreviewObject.GetComponent<MeshRenderer>().SetPropertyBlock(PREVIEW_MPB);
     }
 
     List<Vector3> GetBoundsCorners()
     {
-        Vector3 TopLeft = mInitWorldPos;
-        Vector3 BotRight = mCurrentWorldPos;
+        Vector3 TopLeft = mInitShadowWorldPoint;
+        Vector3 BotRight = mCurrentShadowWorldPoint;
         Vector3 TopRight = new Vector3(BotRight.x, TopLeft.y, TopLeft.z);
         Vector3 BotLeft = new Vector3(TopLeft.x, BotRight.y, BotRight.z);
 
--- Game Jam Openfort 2023/Assets/Shaders/MaskPreview.shader
+++ Game Jam Openfort 2023/Assets/Shaders/MaskPreview.shader
@@ -3,6 +3,7 @@ Shader "Unlit/MaskPreview"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
+        _Noise ("Noise", 2D) = "white" {}
         _HighlightColor ("Highlight Color", Color) = (1,1,1,1)
         _EdgeThickness ("Edge Thickness", Range(0,1)) = 0.1
         _MinAlpha ("Minimum Alpha", Range(0,1)) = 0.3
@@ -38,10 +39,18 @@ Shader "Unlit/MaskPreview"
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
+            sampler2D _Noise;
+            float4 _Noise_ST;
             float4 _HighlightColor;
             float _EdgeThickness;
             float _BlinkSpeed;
             float _MinAlpha;
+
+            float _MaskWidth;
+            float _MaskHeight;
+
+            static const float _RefScreenWidth = 1920.0f;
+            static const float _RefMaskSize = 1.0f;  
 
             Interpolators vert (MeshData v)
             {
@@ -55,18 +64,26 @@ Shader "Unlit/MaskPreview"
             {
                 float Grayscale = 1 - tex2D(_MainTex, i.uv).r;
 
-                float RefWidth = 1920;
-                float ThicknessMultiplier = _ScreenParams.y / RefWidth;
+                float2 NoiseUVs = i.uv * _Noise_ST.xy + _Noise_ST.zw + float2(1,1) * _Time.y * _BlinkSpeed;
+                float NoiseSample = tex2D(_Noise, NoiseUVs).r;
 
-                float Thickness = _EdgeThickness / 2.0f * ThicknessMultiplier;
+                float EdgeScreenSizeMultiplier = _RefScreenWidth / _ScreenParams.y;
+                float EdgeMaskWidthMultiplier = _RefMaskSize / _MaskWidth;                
+                float EdgeMaskHeightMultiplier = _RefMaskSize / _MaskHeight;
 
+                float EdgeCompoMultiplier = EdgeScreenSizeMultiplier * min(EdgeMaskWidthMultiplier, EdgeMaskHeightMultiplier);
+
+                float Thickness = _EdgeThickness / 2.0f * saturate(EdgeCompoMultiplier);
+                // Thickness = 0.1;
                 float AlphaEdge = 0.5f;
 
                 float SDFUnclamped = abs(Grayscale - AlphaEdge);
                 float SDF = 1 - (SDFUnclamped) / (Thickness);
-                float Edge = saturate(SDF);
+                float Edge = saturate(SDF) * saturate(NoiseSample + _MinAlpha);
 
-                float4 Highlight = lerp(_HighlightColor, float4(_HighlightColor.rgb, _MinAlpha), abs(sin(_Time.y * _BlinkSpeed)));
+                float Blink = abs(sin(_Time.y * _BlinkSpeed));
+
+                float4 Highlight = lerp(_HighlightColor, float4(_HighlightColor.rgb, _MinAlpha), Blink);
                 
                 float4 Composition = Edge * Highlight;
 
